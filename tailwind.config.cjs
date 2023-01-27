bd416b034e535b5daea500667984a853808ed0ee
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-27 21:59:21 +0530

    colors and background added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-dark': '	#161722',
+      },
+      backgroundImage: {
+        'hero-pattern-light-mobile': "url('/src/assets/bg-mobile-light.jpg')",
+        'hero-pattern-dark-mobile': "url('/src/assets/bg-mobile-dark.jpg)",
+        'hero-pattern-light-desktop': "url('/src/assets/bg-desktop-light.jpg')",
+        'hero-pattern-dark-desktop': "url('/src/assets/bg-desktop-dark.jpg')",
+      },
+    },
   },
   plugins: [],
 };
