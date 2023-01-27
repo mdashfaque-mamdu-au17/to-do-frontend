9ea44cb1cbf913a661859ce183e55555519b9373
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-27 21:59:44 +0530

    minimum screen and component testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import Hero from './sections/Hero';
+import Navbar from './sections/Navbar';
+import { useGlobalContext } from './store/context';
+import classNames from 'classnames';
 function App() {
+  const { theme } = useGlobalContext();
   return (
-    <div>
-      <h1 className="bg-green-300 text-blue-800">Hello world england</h1>
-    </div>
+    <main
+      className={classNames(
+        'min-h-screen',
+        theme ? 'bg-primary-dark' : 'bg-white'
+      )}
+    >
+      <Hero />
+      <Navbar />
+    </main>
   );
 }
 
