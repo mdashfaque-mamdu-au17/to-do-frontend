ceafbd2c4ea7a2b647cd8e4451e21e940301ce98
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-27 15:30:20 +0530

    initial setup completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-green-300 text-blue-800">Hello world england</h1>
+    </div>
+  );
+}
+
+export default App;
