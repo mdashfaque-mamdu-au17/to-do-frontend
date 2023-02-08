526b1f3815bb23ffb2fb77e11cb135b759c9c96e
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-08 22:44:37 +0530

    hero pattern imported again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
       },
       backgroundImage: {
         'hero-pattern-light-mobile': "url('/src/assets/bg-mobile-light.jpg')",
-        'hero-pattern-dark-mobile': "url('/src/assets/bg-mobile-dark.jpg)",
+        'hero-pattern-dark-mobile': "url('/src/assets/bg-mobile-dark.jpg')",
         'hero-pattern-light-desktop': "url('/src/assets/bg-desktop-light.jpg')",
         'hero-pattern-dark-desktop': "url('/src/assets/bg-desktop-dark.jpg')",
       },
