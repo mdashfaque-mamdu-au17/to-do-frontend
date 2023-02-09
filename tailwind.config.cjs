49cccae074a49b3dd3f2e6561cdc43bb7f7be091
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-09 22:12:18 +0530

    new colors and box shadow added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       colors: {
         'primary-dark': '	#161722',
+        'secondary-dark': '#25273D',
+        'gray-800': '#393A4B',
+        'gray-700': '#767992',
+        'gray-500': '#9495A5',
+        'violet-800': '#C8CBE7',
+      },
+      boxShadow: {
+        'input-light': '0px 35px 50px -15px rgba(194, 195, 214, 0.5)',
+        'input-dark': '0px 35px 50px -15px rgba(0, 0, 0, 0.5)',
       },
       backgroundImage: {
         'hero-pattern-light-mobile': "url('/src/assets/bg-mobile-light.jpg')",
