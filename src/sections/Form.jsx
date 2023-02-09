6dd310afae4d377514f72781fbdef3655fb07ca2
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-09 22:11:54 +0530

    goal input form completed

---
+++ src/sections/Form.jsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useGlobalContext } from '../store/context';
+
+const Form = () => {
+  const { theme } = useGlobalContext();
+
+  const formStyle = theme
+    ? 'bg-secondary-dark placeholder:text-gray-700 text-violet-800 shadow-input-dark'
+    : 'bg-white placeholder:text-gray-500 text-gray-800 shadow-input-light';
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+  }
+  return (
+    <form
+      className={classNames('relative rounded-[5px]')}
+      onSubmit={formSubmitHandler}
+    >
+      <label htmlFor="task">
+        <button
+          type="submit"
+          className={classNames(
+            'w-5 h-5 absolute left-5 top-1/2 transform -translate-y-1/2 lg:h-6 lg:w-6 lg:left-6 rounded-full border-solid border-[1px]',
+            theme && 'border-gray-800'
+          )}
+        ></button>
+        <input
+          type="text"
+          placeholder="Create a new todo…"
+          className={classNames(
+            'w-full h-12 pl-[52px] lg:pl-[72px] lg:h-16 focus:outline-none rounded-[5px] text-xs leading-3 sm:text-lg sm:leading-[18px] sm:-tracking-[0.25px] font-normal',
+            formStyle
+          )}
+        />
+      </label>
+    </form>
+  );
+};
+
+export default Form;
