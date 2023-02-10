08e6313699ab6811c240c752b36eba1b31929b1b
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-10 22:26:25 +0530

    reusalbe tile for showing remaining tasks

---
+++ src/components/Title.jsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useGlobalContext } from '../store/context';
+
+const Title = () => {
+  const { theme } = useGlobalContext();
+  const textStyle = theme ? 'text-gray-200' : 'text-gray-500';
+  return (
+    <p
+      className={classNames(
+        'text-xs leading-3 font-normal sm:text-sm sm:leading-[14px]',
+        textStyle
+      )}
+    >
+      5items left
+    </p>
+  );
+};
+
+export default Title;
