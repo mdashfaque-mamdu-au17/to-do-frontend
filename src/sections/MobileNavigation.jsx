e65cbb222b23fd4ca933dd092291dd36dbe96ec3
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-11 19:39:54 +0530

    navigation component added

---
+++ src/sections/MobileNavigation.jsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React from 'react';
+import CustomLink from '../components/CustomLink';
+import { useGlobalContext } from '../store/context';
+
+const MobileNavigation = () => {
+  const { theme } = useGlobalContext();
+  const containerStyle = theme
+    ? 'bg-secondary-dark shadow-input-dark'
+    : 'bg-white shadow-input-light';
+  return (
+    <section className={classNames(containerStyle, '')}>
+      <ul className="pt-[15px] pb-[19px] flex justify-center items-center gap-[18px]">
+        <li>
+          <CustomLink to="/">All</CustomLink>
+        </li>
+        <li>
+          <CustomLink to="activeTasks">Active</CustomLink>
+        </li>
+        <li>
+          <CustomLink to="completedTasks">Completed</CustomLink>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default MobileNavigation;
