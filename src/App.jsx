4662102c2a187d9bb1757e6c5f337d9e28111411
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-11 19:41:01 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
 import { useGlobalContext } from './store/context';
 import classNames from 'classnames';
 import RootLayout from './pages/RootLayout';
+import AllTasks from './pages/AllTasks';
+import ActiveTasks from './pages/ActiveTasks';
+import CompletedTasks from './pages/CompletedTasks';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <AllTasks /> },
+      { path: 'activeTasks', element: <ActiveTasks /> },
+      { path: 'completedTasks', element: <CompletedTasks /> },
+    ],
+  },
+]);
+
 function App() {
   const { theme } = useGlobalContext();
   return (
@@ -15,7 +32,7 @@ function App() {
     >
       <Hero />
       <Navbar />
-      <RootLayout />
+      <RouterProvider router={router} />
     </main>
   );
 }
