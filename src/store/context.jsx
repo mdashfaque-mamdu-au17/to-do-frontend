a1566bc58bd8da83b18c62b88321d8504018ea87
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-14 23:26:43 +0530

    fetchalltasks, and create new task handlers

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 
+let baseUrl = 'https://to-do-api-t50i.onrender.com/api/v1';
 const AppContext = React.createContext();
 
 const getStorageTheme = () => {
@@ -11,8 +12,13 @@ const getStorageTheme = () => {
 
   return theme === 'true' ? true : false;
 };
+
 const AppProvider = ({ children }) => {
   const [theme, setTheme] = useState(getStorageTheme());
+  const [allTasks, setAllTasks] = useState([]);
+  const [allTasksLoading, setAllTasksLoading] = useState(false);
+
+  const allTasksCount = allTasks.length;
 
   const changeTheme = () => {
     setTheme(!theme);
@@ -21,8 +27,50 @@ const AppProvider = ({ children }) => {
   useEffect(() => {
     localStorage.setItem('theme', theme);
   }, [theme]);
+
+  const fetchAllTasks = async () => {
+    try {
+      setAllTasksLoading(true);
+      const data = await fetch(`${baseUrl}/tasks`);
+      const result = await data.json();
+      setAllTasks(result.tasks);
+      setAllTasksLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, []);
+
+  const createNewTaskHandler = async (newTask) => {
+    try {
+      const response = await fetch(`${baseUrl}/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: newTask }),
+      });
+      console.log(response);
+      fetchAllTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <AppContext.Provider value={{ theme, changeTheme }}>
+    <AppContext.Provider
+      value={{
+        theme,
+        changeTheme,
+        allTasks,
+        allTasksLoading,
+        allTasksCount,
+        createNewTaskHandler,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
