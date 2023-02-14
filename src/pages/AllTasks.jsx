9074f8761daba7e48a283e6c8e3def4aa76be3f9
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-14 23:25:42 +0530

    all tasks from api

--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -1,19 +1,21 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleTask from '../components/SingleTask';
 import { useGlobalContext } from '../store/context';
 
 const AllTasks = () => {
-  const { theme } = useGlobalContext();
+  const { theme, allTasks, allTasksLoading } = useGlobalContext();
 
   const containerStyle = theme ? 'bg-secondary-dark' : 'bg-white';
+
   return (
     <section className={classNames(containerStyle)}>
-      <SingleTask name="Complete online JavaScript course" completed={true} />
-      <SingleTask name="Jog around the park 3x" completed={false} />
-      <SingleTask name="10 minutes meditation" completed={false} />
-      <SingleTask name="Read for 1 hour" completed={false} />
-      <SingleTask name="Pickup groceries" completed={true} />
+      {allTasksLoading && <h1>Loading...</h1>}
+      {allTasks.length < 1 && <p>No Taks Found.</p>}
+      {!allTasksLoading &&
+        allTasks.map((singleTask) => {
+          return <SingleTask {...singleTask} key={singleTask?._id} />;
+        })}
     </section>
   );
 };
