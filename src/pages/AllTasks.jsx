d8d14c7888a2adaf64da5f1006999ee3c6b5b142
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-18 21:08:36 +0530

    all task showing not found logic changed

--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -1,17 +1,23 @@
 import classNames from 'classnames';
 import React, { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import NoTaskFound from '../components/NoTaskFound';
 import SingleTask from '../components/SingleTask';
 import { useGlobalContext } from '../store/context';
 
 const AllTasks = () => {
-  const { theme, allTasks, allTasksLoading } = useGlobalContext();
+  const { theme, allTasks, allTasksLoading, fetchAllTasks } =
+    useGlobalContext();
 
   const containerStyle = theme ? 'bg-secondary-dark' : 'bg-white';
 
+  useEffect(() => {
+    fetchAllTasks();
+  }, []);
   return (
     <section className={classNames(containerStyle)}>
-      {allTasksLoading && <h1>Loading...</h1>}
-      {allTasks.length < 1 && <p>No Taks Found.</p>}
+      {allTasksLoading && <Loading />}
+      {allTasks.length < 1 && !allTasksLoading && <NoTaskFound />}
       {!allTasksLoading &&
         allTasks.map((singleTask) => {
           return <SingleTask {...singleTask} key={singleTask?._id} />;
