6ebc785f0a495837e36ecd02172e54dde82c0937
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-02-18 21:08:18 +0530

    active task and completed task pages created

--- src/pages/ActiveTasks.jsx
+++ src/pages/ActiveTasks.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useEffect } from 'react';
+import SingleTask from '../components/SingleTask';
+import Loading from '../components/Loading';
+import NoTaskFound from '../components/NoTaskFound';
+import { useGlobalContext } from '../store/context';
 
 const ActiveTasks = () => {
-  return <div>ActiveTasks</div>;
+  const { theme, fetchActiveTasks, activeTasks, activeTasksLoading } =
+    useGlobalContext();
+
+  useEffect(() => {
+    fetchActiveTasks();
+  }, []);
+  const containerStyle = theme ? 'bg-secondary-dark' : 'bg-white';
+  return (
+    <section className={classNames(containerStyle)}>
+      {activeTasksLoading && <Loading />}
+      {activeTasks.length < 1 && !activeTasksLoading && <NoTaskFound />}
+
+      {!activeTasksLoading &&
+        activeTasks.map((singleTask) => {
+          return <SingleTask {...singleTask} key={singleTask?._id} />;
+        })}
+    </section>
+  );
 };
 
 export default ActiveTasks;
--- src/pages/CompletedTasks.jsx
+++ src/pages/CompletedTasks.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useEffect } from 'react';
+import SingleTask from '../components/SingleTask';
+import NoTaskFound from '../components/NoTaskFound';
+import Loading from '../components/Loading';
+import { useGlobalContext } from '../store/context';
 
 const CompletedTasks = () => {
-  return <div>CompletedTasks</div>;
+  const { theme, completedTasks, completedTasksLoading, fetchCompletedTasks } =
+    useGlobalContext();
+
+  const containerStyle = theme ? 'bg-secondary-dark' : 'bg-white';
+
+  useEffect(() => {
+    fetchCompletedTasks();
+  }, []);
+  return (
+    <section className={classNames(containerStyle)}>
+      {completedTasksLoading && <Loading />}
+      {completedTasks.length < 1 && !completedTasksLoading && <NoTaskFound />}
+
+      {!completedTasksLoading &&
+        completedTasks.map((singleTask) => {
+          return <SingleTask {...singleTask} key={singleTask?._id} />;
+        })}
+    </section>
+  );
 };
 
 export default CompletedTasks;
